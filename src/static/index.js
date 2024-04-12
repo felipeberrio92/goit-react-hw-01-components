ed14eb9f1cbaee1f6b73a524f34f6b3356e42f17
felipeberrio92 <felipeberriomarin92@gmail.com>
2024-04-12 11:11:49 -0500

    se arregla la configuracion de la pagina

---
+++ src/static/index.js
@@ -0,0 +1,11 @@
+import USER from './user.json'
+import TRANSACTIONS from './transactions.json'
+import FRIENDS from './friends.json'
+import DATA from './data.json'
+
+export {
+    USER,
+    TRANSACTIONS,
+    FRIENDS,
+    DATA
+}
